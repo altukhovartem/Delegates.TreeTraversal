3e99400d81e06c39d70145f79a82ddc2efa50bf0
Artem <altukhov.artem.dev@gmain.com>
2018-03-28 23:39:09 +0300

    still not done

--- Program.cs
+++ Program.cs
@@ -1,12 +1,43 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using Delegates.TreeTraversal;
 using NUnitLite;
 
 class Program
 {
 	static void Main(string[] args)
 	{
-		new AutoRun().Execute(args);
+		var data = new Job
+		{
+			Name = "4",
+			Subjobs = new List<Job>
+				  {
+					  new Job
+					  {
+						  Name="3"
+					  },
+					  new Job
+					  {
+						  Name="A",
+						  Subjobs=new List<Job>
+						  {
+							  new Job
+							  {
+								  Name="1"
+							  },
+							  new Job
+							  {
+								  Name="2"
+							  }
+						  }
+
+					  }
+				  }
+		};
+
+		var x = Traversal.GetEndJobs(data);
+
+		//new AutoRun().Execute(args);
 	}
 }
--- Traversal.cs
+++ Traversal.cs
@@ -10,13 +10,14 @@ namespace Delegates.TreeTraversal
 	{
 		public static IEnumerable<int> GetBinaryTreeValues(BinaryTree<int> tree)
 		{
-			return Travel
-			(
-				tree,
-				t => t,
-				j => j.Left 
-				j => j.Name
-			);
+			throw new NotImplementedException();
+			//return Travel
+			//(
+			//	tree,
+			//	t => t,
+			//	j => j.Left 
+			//	j => j.Name
+			//);
 		}
 
 		public static IEnumerable<Job> GetEndJobs(Job tree)
@@ -25,25 +26,29 @@ namespace Delegates.TreeTraversal
 			(
 				tree,
 				j => j.Subjobs,
-				j => j.Subjobs == null,
-				j => j.Name
+				j => j.Subjobs.Count == 0,
+				j => j
 			);
 		}
 
 		public static IEnumerable<Product> GetProducts(ProductCategory tree)
 		{
-			return Travel
-			(
-				tree,
-				j => j.Categories,
-				j => j.Categories == null,
-				j => j.Products.Select(p => p.Name)
-			);
+			throw new NotImplementedException();
+			//return Travel
+			//(
+			//	tree,
+			//	j => j.Categories,
+			//	j => j.Categories == null,
+			//	j => j.Products.Select(p => p.Name)
+			//);
 		}
 
-		public static IEnumerable<Tin> Travel<Tin, Tout>(Tin root, Func<Tin,IEnumerable<Tin>> childrenSelector, Func<Tin,bool> filter, Func<Tin, Tout> resultSelector)
+		public static IEnumerable<Tout> Travel<Tin, Tout>(Tin root, Func<Tin,IEnumerable<Tin>> childrenSelector, Func<Tin,bool> filter, Func<Tin, Tout> resultSelector)
 		{
-			throw new NotImplementedException();
+			var selector = childrenSelector(root).Where(filter).SelectMany(n => Travel(n, childrenSelector, filter, resultSelector));
+			var selector2 = Enumerable.Concat(childrenSelector(root).SelectMany(resultSelector))
+				
+				return childrenSelector.Invoke(root).Where(filter).Select(resultSelector); 
 		}
 	}
 
